44316237c8b2aa39c3837129c1f789f1f21543a7
Noahnoah55 <giantnoah@gmail.com>
2018-03-23 22:46:24 -0400

    Finished Character Movement

--- Scenes/GameParts/Player.gd
+++ Scenes/GameParts/Player.gd
@@ -1,11 +1,19 @@
 extends KinematicBody2D
 var velocity = Vector2()
-export var gravity = 400
-export var jumpforce = 100
+export var gravity = 500
+export var jumpforce = 300
+export var walkspeed = 300
+export var aircontrol = 400
 export var upbutton = 'ui_up'
-export var downbutton = 'ui_up'
-export var leftbutton = 'ui_up'
-export var rightbutton = 'ui_up'
+export var downbutton = 'ui_down'
+export var leftbutton = 'ui_left'
+export var rightbutton = 'ui_right'
+var direction = Vector2()
+var throw = 0
+var b = 0
+var jumpsleft = 2
+var isonground = false
+export var debug = true
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -16,11 +24,24 @@ func _ready():
 	pass
 
 func _process(delta):
+	direction = Vector2()
+	isonground = false
 	velocity.y += gravity * delta
-	for i in $Area2D.get_overlapping_bodies():
-		if i.is_in_group('Block'):
+	b = $Area2D.get_overlapping_bodies()
+	for i in range(0, b.size()):
+		if b[i].is_in_group('Block'):
 			velocity.y = 0
-	if Input.is_action_just_pressed('ui_up'):
-		velocity.y = jumpforce
-		Label.Text = 'jump'
+			jumpsleft = 2
+			isonground = true
+	if Input.is_action_just_pressed(upbutton):
+		if jumpsleft > 0:
+			velocity.y = -jumpforce
+			jumpsleft -= 1
+	if Input.is_action_pressed(leftbutton):
+		direction.x -= 1
+	if Input.is_action_pressed(rightbutton):
+		direction.x += 1
+	velocity.x = direction.x * walkspeed
 	move_and_slide(velocity)
+	if debug:
+		$Label.text = String(velocity) + String(isonground)
