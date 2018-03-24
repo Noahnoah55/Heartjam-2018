376540acd0972197817d229ee02acc3a9c0dbdf7
Noahnoah55 <giantnoah@gmail.com>
2018-03-24 00:00:16 -0400

    Movement fixes, player bouncing, and physics refactoring

--- Scenes/GameParts/Player.gd
+++ Scenes/GameParts/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
+var jumpforce
+var walkspeed
+var aircontrol
+var jumpbonus
+var gravity
+var bouncespeed
 var velocity = Vector2()
-export var gravity = 500
-export var jumpforce = 300
-export var walkspeed = 300
-export var aircontrol = 400
+var prepos
+export var jumpbutton = 'ui_jump'
 export var upbutton = 'ui_up'
 export var downbutton = 'ui_down'
 export var leftbutton = 'ui_left'
@@ -16,30 +20,58 @@ var jumpsleft = 2
 var isonground = false
 export var debug = true
 var facing = Vector2()
+var lastx = 0
 
 func _ready():
-	pass
+	jumpforce = get_node('/root/Level').jumpforce
+	walkspeed = get_node('/root/Level').walkspeed
+	aircontrol = get_node('/root/Level').aircontrol
+	jumpbonus = get_node('/root/Level').jumpbonus
+	gravity = get_node('/root/Level').gravity
+	bouncespeed = get_node('/root/Level').bouncespeed
 
 func _process(delta):
 	direction = Vector2()
+	facing = Vector2()
 	isonground = false
-	velocity.y += gravity * delta
-	b = $Area2D.get_overlapping_bodies()
+	if Input.is_action_pressed(jumpbutton):
+		velocity.y += gravity * delta
+	else:
+		velocity.y += gravity * delta * jumpbonus
+	b = $Floor.get_overlapping_bodies()
 	for i in range(0, b.size()):
 		if b[i].is_in_group('Block'):
 			velocity.y = 0
 			jumpsleft = 2
 			isonground = true
-	if Input.is_action_just_pressed(upbutton):
+		if b[i].is_in_group('Player'):
+			if b[i] != self:
+				velocity.y = -bouncespeed
+			
+	if Input.is_action_just_pressed(jumpbutton):
 		if jumpsleft > 0:
 			velocity.y = -jumpforce
 			jumpsleft -= 1
 	if Input.is_action_pressed(leftbutton):
 		direction.x -= 1
-		facing.x
+		facing.x = -1
+		lastx = -1
 	if Input.is_action_pressed(rightbutton):
 		direction.x += 1
+		facing.x
+		lastx = 1
+	if Input.is_action_pressed(upbutton):
+		facing.y = -1
+	elif Input.is_action_pressed(downbutton):
+		facing.y = 1
+	else:
+		facing.x = lastx
+
+
 	velocity.x = direction.x * walkspeed
+	prepos = position
 	move_and_slide(velocity)
+	velocity = (position - prepos)/delta
 	if debug:
-		$Label.text = String(velocity) + String(isonground)
+#		$Label.text = String(velocity) + String(isonground) + String(facing)
+		pass
